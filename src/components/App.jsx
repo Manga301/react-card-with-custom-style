e09da135b4379fed6ae8dfbcd86200511d2ed120
Mangaliso <mangaliso.twala@gmail.com>
2022-01-01 09:35:05 +0200

    Add contacts map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,28 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+// create a new card that you can loop through
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar imgURL="https://avatars.githubusercontent.com/Manga301" />
-      <Card
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         imgURL={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -25,7 +41,7 @@ function App() {
         imgURL={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,17 @@ import Avatar from "./Avatar";
 import Details from "./Details";
 
 function Card(props) {
+  console.log(props);
+
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
+
         <Avatar imgURL={props.imgURL} />
       </div>
       <div className="bottom">
+        <Details detailInfo={props.id} />
         <Details detailInfo={props.tel} />
         <Details detailInfo={props.email} />
       </div>
